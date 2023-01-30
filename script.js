6c2a3449802b9e9e8a9eb3183ff48132cb57f53f
electi <barankircaa@gmail.com>
2023-01-31 01:29:47 +0300

    complete rps-gui

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 // store computer score and player score
 let computerScore = 0;
 let playerScore = 0;
+let ties = 0;
+let roundsPlayed = 0;
+let page = document.querySelector(".page-container");
 
-game();
+// take user input (rock-paper-scissors)
+let userInput;
+const buttonList = document.querySelectorAll(".button");
+buttonList.forEach((button) =>
+    button.addEventListener("click", (e) => playRound(e, button))
+);
 
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     let computerChoice = Math.floor(Math.random() * 3);
-    return options[computerChoice].charAt(0).toLowerCase();
+    return options[computerChoice];
 }
 
-function playRound(playerChoice, computerChoice) {
+function determineRoundWinner(playerChoice, computerChoice) {
     switch (playerChoice) {
         case "r":
             switch (computerChoice) {
                 case "r":
-                    console.log("It's a tie");
+                    ties++;
                     break;
                 case "p":
-                    console.log("Computer won the round");
                     computerScore++;
                     break;
                 case "s":
-                    console.log("You won the round");
                     playerScore++;
                     break;
             }
@@ -30,14 +36,12 @@ function playRound(playerChoice, computerChoice) {
         case "p":
             switch (computerChoice) {
                 case "r":
-                    console.log("You won the round");
                     playerScore++;
                     break;
                 case "p":
-                    console.log("It's a tie");
+                    ties++;
                     break;
                 case "s":
-                    console.log("Computer won the round");
                     computerScore++;
                     break;
             }
@@ -45,40 +49,117 @@ function playRound(playerChoice, computerChoice) {
         case "s":
             switch (computerChoice) {
                 case "r":
-                    console.log("Computer won the round");
                     computerScore++;
                     break;
                 case "p":
-                    console.log("You won the round");
                     playerScore++;
                     break;
                 case "s":
-                    console.log("It's a tie!");
+                    ties++;
                     break;
             }
             break;
-        default:
-            console.log(
-                "Computer won the round because you've entered an invalid value."
-            );
-            computerScore++;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // take user input (rock-paper-scissors)
-        let userInput = prompt("Rock, paper, or scissors? ")
-            .charAt(0)
-            .toLowerCase();
-        // let computer play
-        playRound(userInput, getComputerChoice());
-        // determine winner
-        // keep playing until score reaches 5
-    }
-    if (playerScore > computerScore) {
-        console.log("You won the game. Congrats!");
+async function playRound(e, button) {
+    disableButtons();
+    e.stopPropagation();
+    roundsPlayed++;
+    // let computer play
+    let computerChoice = getComputerChoice();
+    // determine winner
+    determineRoundWinner(button.id.charAt(0), computerChoice.charAt(0));
+    //update the scoreboard
+    updateScoreBoard();
+    //keep playing until 10 rounds are played
+    if (roundsPlayed === 10) {
+        await sleep(1000);
+        computerScore > playerScore
+            ? announceWinner("Computer")
+            : playerScore > computerScore
+            ? announceWinner("Player")
+            : announceWinner("Tie");
+        onGameEnd();
     } else {
-        console.log("Computer won the game. Pray for a better luck next time!");
+        enableButtons();
     }
+}
+
+function announceWinner(winner) {
+    let winnerFeedBack = document.createElement("h1");
+    let classList = ["game-ended", "winner-feedback"];
+    classList.forEach((_class) => {
+        winnerFeedBack.classList.add(_class);
+    });
+    winnerFeedBack.textContent =
+        winner !== "Tie" ? `${winner.toUpperCase()} WINS!` : "IT'S A TIE!";
+    page.appendChild(winnerFeedBack);
+}
+
+function updateScoreBoard() {
+    let scoreTexts = document.querySelectorAll(".score-text");
+    let score;
+    scoreTexts.forEach((scoreText) => {
+        switch (scoreText.id) {
+            case "player-score":
+                score = playerScore;
+                break;
+            case "computer-score":
+                score = computerScore;
+                break;
+            case "ties":
+                score = ties;
+                break;
+        }
+        scoreText.querySelector(".score").textContent = score;
+    });
+}
+
+function disableButtons() {
+    buttonList.forEach((button) => {
+        button.disabled = true;
+        button.classList.remove("working");
+    });
+}
+
+function enableButtons() {
+    buttonList.forEach((button) => {
+        button.disabled = false;
+        button.classList.add("working");
+    });
+}
+
+function onGameEnd() {
+    page.querySelectorAll("*").forEach((element) => {
+        if (!element.classList.contains("game-ended")) {
+            element.style.visibility = "hidden";
+        }
+    });
+    let playAgainButton = document.createElement("button");
+    playAgainButton.classList.add("game-ended");
+    playAgainButton.textContent = "PLAY AGAIN";
+    playAgainButton.addEventListener("click", (e) => restartGame(e));
+    page.appendChild(playAgainButton);
+}
+
+function restartGame(e) {
+    e.stopPropagation();
+    computerScore = 0;
+    playerScore = 0;
+    ties = 0;
+    roundsPlayed = 0;
+    updateScoreBoard();
+    enableButtons();
+    page.querySelectorAll("*").forEach((element) => {
+        if (!element.classList.contains("game-ended")) {
+            element.style.visibility = "visible";
+        } else {
+            page.removeChild(element);
+        }
+    });
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
